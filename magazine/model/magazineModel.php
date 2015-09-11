1cbbb501f231f0c6d68ea5194ce5bc292ffe63a1
Ronak Shah <ronak.shah@dalaltimes.com>
2015-09-11 12:34:16 +0530

    started the logic for sending mailers

--- magazine/model/magazineModel.php
+++ magazine/model/magazineModel.php
@@ -833,7 +833,7 @@ class magazineModel extends Database {
 	protected function getAboutSection() {
 		return array('img' => 'public/images/magazine_cover.jpg', 'content' => '<p>Dalal Times Magazine, a monthly publication, takes into account events, news and views of an entire month and helps translate them in to its impact on the share market.</p>
           <p>We leverage this form of media to reach out to our readers who not only constitute of investors and traders but also keen individuals who desire to benefit from our teams research and analytics expertise in equity market.</p>
-          <p>Our aim is to keep you ahead of the market fluctuations and stay true to the magazine’s motto ‘Voice Of The Indian Stock Market’.</p>', );
+          <p>Our aim is to keep you ahead of the market fluctuations and stay true to the magazine’s motto ‘Voice Of The Indian Stock Market’.</p>');
 	}
 
 	protected function getWhyDtSection() {
@@ -1371,14 +1371,13 @@ class magazineModel extends Database {
 					$this->bindByValue('updtmonths', $pkg_details[0]['no_of_months']);
 					if ($this->execute()) {
 						$this->endTransaction();
-						if (base64_decode($_SESSION['_uid']) == 14) {
+						if (isset($_SESSION['q'])) {
 							$this->_modelQuery = "UPDATE expired_users set expired_user_subscribed=1 where expired_user_unique_id = '" . $_SESSION['q'] . "'";
 							$this->query($this->_modelQuery);
 							$this->execute();
 							unset($_SESSION['q']);
-
-							$this->sendMagazineSubscriptionMailer($mailerDetails);
 						}
+						$this->sendMagazineSubscriptionMailer($mailerDetails);
 						return base64_encode($parameter['order_id']);
 					} else {
 						$this->cancelTransaction();
