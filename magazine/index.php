8d0217044ecee9cad5f26f9d881aafcf78ea7694
Ronak Shah <ronak.shah@dalaltimes.com>
2015-11-13 16:36:43 +0530

    Free link for the samvat Issue

--- magazine/index.php
+++ magazine/index.php
@@ -33,6 +33,7 @@ $_router->map('GET', '/payment-error', 'magazine#gatewayResponseError');
 $_router->map('GET', '/sneak-preview/[a:issue]', 'magazine#showPartialBook');
 $_router->map('GET', '/magazine/[a:issue]', 'magazine#showSubscribedBook');
 $_router->map('GET', '/samvat/[a:issue]', 'magazine#showSpecialIssue');
+$_router->map('GET', '/free-samvat/[a:issue]', 'magazine#showFreeSpecialIssue');
 $_router->map('GET', '/view/document', 'magazine#showDocument');
 
 $controller_name = null;
